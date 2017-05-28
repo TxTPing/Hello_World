489c435e1d56c65115f3116a15c25f549f33b62b
TxTPing <cra01@txtping.com>
2017-05-28 20:11:22 +0100

    Update chaincode.go

--- chaincode.go
+++ chaincode.go
@@ -88,10 +88,10 @@ return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the 
 
 key = args[0] //rename for funsies
 value = args[1]
-chaincodeName = "github.com/TxTPing/Hello_World2"
+chaincodeName := []string("github.com/TxTPing/Hello_World2")
  
 if args[1] == "drop"{
-response = stub.InvokeChaincode([]byte(chaincodeName), "")
+response = stub.InvokeChaincode(chaincodeName, "")
 }else{
 err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 }
